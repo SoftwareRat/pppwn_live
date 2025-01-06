b7dfb5fc017ff8d30d53fbaa7fa2f13bb5e12329
SoftwareRat <40393784+SoftwareRat@users.noreply.github.com>
2025-01-06 02:11:04 +0100

    chore: update PPPwn dependencies and improve binary extraction process

--- package/pppwn/pppwn.mk
+++ package/pppwn/pppwn.mk
@@ -9,7 +9,7 @@ PPPWN_SITE_METHOD = local
 PPPWN_SITE = $(BR2_EXTERNAL_PPPWN_LIVE_PATH)/package/pppwn/binaries
 PPPWN_LICENSE = GPL-3.0
 PPPWN_LICENSE_FILES = LICENSE
-PPPWN_DEPENDENCIES = host-jq
+PPPWN_DEPENDENCIES = host-jq host-wget host-curl host-p7zip
 
 # Define architecture-specific zip file
 ifeq ($(BR2_x86_64),y)
@@ -19,11 +19,10 @@ PPPWN_BINARY_ZIP = aarch64-linux-musl.zip
 endif
 
 define PPPWN_EXTRACT_CMDS
-    # Extract PPPwn binary
-    $(UNZIP) -d $(@D) $(PPPWN_SITE)/$(PPPWN_BINARY_ZIP)
-    tar xf $(@D)/pppwn.tar.gz -C $(@D)
+    # Extract PPPwn binary (two-step extraction: zip -> tar.gz -> binary)
+    $(UNZIP) -o -d $(@D) $(PPPWN_SITE)/$(PPPWN_BINARY_ZIP)
+    cd $(@D) && tar xf pppwn.tar.gz
     
-    ## For copyright reasons, we need to download the stage1.bin and stage2.bin files separately
     # Download stage1.bin
     $(HOST_DIR)/bin/wget -O $(@D)/stage1.bin \
         "https://github.com/B-Dem/PPPwnUI/raw/main/PPPwn/goldhen/1100/stage1.bin"
@@ -34,7 +33,7 @@ define PPPWN_EXTRACT_CMDS
     cd $(@D) && $(HOST_DIR)/bin/7z e GoldHEN.7z pppnw_stage2/stage2_v*.7z -r -aoa
     cd $(@D) && $(HOST_DIR)/bin/7z e stage2_v*.7z stage2_11.00.bin -r -aoa
     cd $(@D) && mv stage2_11.00.bin stage2.bin
-    cd $(@D) && rm -f GoldHEN.7z stage2_v*.7z
+    cd $(@D) && rm -f GoldHEN.7z stage2_v*.7z pppwn.tar.gz
 endef
 
 define PPPWN_INSTALL_TARGET_CMDS
