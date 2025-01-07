5ed5901d6ba5be5ec157ad02a360263d23cf6c0c
SoftwareRat <40393784+SoftwareRat@users.noreply.github.com>
2025-01-07 21:59:39 +0100

    - Temp WAR: Just creating grub config inside the Linux RAM image - FIx graphical bugs with banner

--- board/pppwn/post-build.sh
+++ board/pppwn/post-build.sh
@@ -3,5 +3,25 @@ set -e
 
 chmod +x "${TARGET_DIR}/etc/init.d/S50pppwn"
 
-# Remove conflicting grub.cfg from target directory
-rm -f "${TARGET_DIR}/boot/grub/grub.cfg"
+# Ensure boot/grub directory exists
+mkdir -p "${TARGET_DIR}/boot/grub"
+
+# Create the correct grub.cfg for initrd
+cat > "${TARGET_DIR}/boot/grub/grub.cfg" << EOF
+set default="0"
+set timeout="5"
+
+if [ "\${grub_platform}" = "efi" ]; then
+    set prefix=(cd0)/boot/grub
+    menuentry "PPPwnLive" {
+        linux (cd0)/boot/bzImage root=/dev/ram0 console=tty1
+        initrd (cd0)/boot/initrd
+    }
+else
+    set prefix=(cd)/boot/grub
+    menuentry "PPPwnLive" {
+        linux (cd)/boot/bzImage root=/dev/ram0 console=tty1
+        initrd (cd)/boot/initrd
+    }
+fi
+EOF
